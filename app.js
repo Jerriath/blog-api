bac99183712cbb16bea529347f669542a309f5a2
Jerry Zhang <jerryzhang101@yahoo.com>
2022-06-06 00:53:50 -0400

    Added passport.use on app.js for signing up

--- app.js
+++ app.js
@@ -2,9 +2,32 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 // Importing the api router
 const apiRouter = require('./routes/api');
+
+// Importing models
+const User = require('./models/user');
+
+// Setting up passport middleware
+passport.use('login', new LocalStrategy(
+    {
+        'usernameField': 'username',
+        'passwordField': 'password'
+    },
+    (username, password, done) => {
+        const newUser = new User({
+            username,
+            password
+        });
+        newUser.save((err) => {
+            return next(err);
+        });
+        return done(null, newUser);
+    }
+))
 
 // Initializing our app variable
 const app = express();
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,38 @@
 // Importing model
-const users = require('../models/user');
+const Users = require('../models/user');
 
 // Importing necessary node modules
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const { body, validationResult } = require('express-validator');
+
+// Exporting controller middleware
+exports.user_create_post = [
+    body('username')
+        .trim()
+        .isLength({ min: 1})
+        .escape(),
+    body('password')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('confirmation')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .custom((confirmation, { req }) => {
+            if (confirmation !== req.body.password) { return next(new Error("Password did not match confirmation"))}
+            else { return true; }
+        }),
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                res.json({
+                    username: username,
+                    errors
+                });
+            }
+            
+        }
+]
